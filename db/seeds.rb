e3d590c19b975f9a6ca83123497c9d098c2aa9a7
Mae Schreck <maedevlin47@gmail.comm>
2022-12-12 17:49:12 -0500

    commit tree 2fc03ee18ebaae090cc5103910eb136788e150fc

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.destroy_all
+
+Student.create(first_name: 'Bob', last_name: 'Smith', grade: 1)
+Student.create(first_name: 'Sally', last_name: 'Right', grade: 2)
+Student.create(first_name: 'Tommy', last_name: 'Wrong', grade: 3)
+Student.create(first_name: 'Dwayne', last_name: 'Johnson', grade: 3)
